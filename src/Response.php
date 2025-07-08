590ce9c40dc1b12a09ae78f04c32b41d0df8dc8e
Blain Armstrong <blainarm387@gmail.com>
2025-07-08 23:02:24 +0900

    feat: Simplify response

--- src/Response.php
+++ src/Response.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 namespace Laminas\ReCaptcha;
 
-use Laminas\Http\Response as HTTPResponse;
+use Psr\Http\Message\ResponseInterface;
 
 use function array_key_exists;
 use function is_array;
-use function is_string;
 use function json_decode;
 use function trim;
 
@@ -21,68 +20,26 @@ use const JSON_THROW_ON_ERROR;
  */
 class Response
 {
-    /**
-     * Status
-     *
-     * true if the response is valid or false otherwise
-     *
-     * @var bool
-     */
-    protected $status;
+    private bool $isValid;
 
-    /**
-     * Error codes
-     *
-     * The error codes if the status is false. The different error codes can be found in the
-     * recaptcha API docs.
-     *
-     * @var array
-     */
-    protected $errorCodes = [];
+    private array $errorCodes;
 
-    /**
-     * Class constructor used to construct a response
-     *
-     * @param bool|null $status
-     * @param array $errorCodes
-     * @param null|HTTPResponse $httpResponse If this is set the content will override $status and $errorCode
-     */
-    public function __construct($status = null, $errorCodes = [], ?HTTPResponse $httpResponse = null)
+    public function __construct(ResponseInterface $response)
     {
-        if ($status !== null) {
-            $this->setStatus($status);
+        $body = (string)$response->getBody();
+        $parts = trim($body) === '' ? [] : json_decode($body, true, flags: JSON_THROW_ON_ERROR);
+
+        $this->isValid = false;
+        $errorCodes = [];
+
+        if (is_array($parts) && array_key_exists('success', $parts)) {
+            $this->isValid = $parts['success'];
+            if (array_key_exists('error-codes', $parts)) {
+                $errorCodes = $parts['error-codes'];
+            }
         }
 
-        if (! empty($errorCodes)) {
-            $this->setErrorCodes($errorCodes);
-        }
-
-        if ($httpResponse !== null) {
-            $this->setFromHttpResponse($httpResponse);
-        }
-    }
-
-    /**
-     * Set the status
-     *
-     * @param bool $status
-     * @return self
-     */
-    public function setStatus($status)
-    {
-        $this->status = (bool) $status;
-
-        return $this;
-    }
-
-    /**
-     * Get the status
-     *
-     * @return bool
-     */
-    public function getStatus()
-    {
-        return $this->status;
+        $this->errorCodes = $errorCodes;
     }
 
     /**
@@ -90,26 +47,9 @@ class Response
      *
      * @return bool
      */
-    public function isValid()
+    public function isValid(): bool
     {
-        return $this->getStatus();
-    }
-
-    /**
-     * Set the error codes
-     *
-     * @param string|array $errorCodes
-     * @return self
-     */
-    public function setErrorCodes($errorCodes)
-    {
-        if (is_string($errorCodes)) {
-            $errorCodes = [$errorCodes];
-        }
-
-        $this->errorCodes = $errorCodes;
-
-        return $this;
+        return $this->isValid;
     }
 
     /**
@@ -117,34 +57,8 @@ class Response
      *
      * @return array
      */
-    public function getErrorCodes()
+    public function getErrorCodes(): array
     {
         return $this->errorCodes;
-    }
-
-    /**
-     * Populate this instance based on a Laminas_Http_Response object
-     *
-     * @return self
-     */
-    public function setFromHttpResponse(HTTPResponse $response)
-    {
-        $body  = $response->getBody();
-        $parts = '' !== trim($body) ? json_decode($body, true, 512, JSON_THROW_ON_ERROR) : [];
-
-        $status     = false;
-        $errorCodes = [];
-
-        if (is_array($parts) && array_key_exists('success', $parts)) {
-            $status = $parts['success'];
-            if (array_key_exists('error-codes', $parts)) {
-                $errorCodes = $parts['error-codes'];
-            }
-        }
-
-        $this->setStatus($status);
-        $this->setErrorCodes($errorCodes);
-
-        return $this;
     }
 }
